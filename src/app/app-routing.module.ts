deb4a43f33145b69bb500dc149b6a5cf2d6d8f4d
Rei Lin <temisrei@gmail.com>
2017-08-05 09:38:22 +0800

    修改 app-routing 路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component }
 ];
 
 @NgModule({
