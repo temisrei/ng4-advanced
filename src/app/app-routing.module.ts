849fac7082703d31ef01933bade8291294a2fa3c
Rei Lin <temisrei@gmail.com>
2017-08-05 10:47:22 +0800

    定義萬用路由，必須放在最後一個路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { Page2Component } from './page2/page2.component';
 const routes: Routes = [
   { path: '', redirectTo: '/page1', pathMatch: 'full' },
   { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component }
+  { path: 'page2', component: Page2Component },
+  { path: '**', redirectTo: '/', pathMatch: 'full'}
 ];
 
 @NgModule({
