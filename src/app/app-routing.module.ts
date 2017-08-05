dfe0248af0b085a98b3fef94dc32e762146f9e1a
Rei Lin <temisrei@gmail.com>
2017-08-05 10:49:30 +0800

    定義獨立的 Route 物件，抽出常用的共同物件。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
+const fallbackRoute = {
+  path: '**',
+  redirectTo: '/',
+  pathMatch: 'full'
+};
+
 const routes: Routes = [
   { path: '', redirectTo: '/page1', pathMatch: 'full' },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component },
-  { path: '**', redirectTo: '/', pathMatch: 'full'}
+  fallbackRoute
 ];
 
 @NgModule({
