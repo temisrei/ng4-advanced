67185d496cdf1a65f790667a27deac2cd4d3e7be
Rei Lin <temisrei@gmail.com>
2017-08-05 15:43:20 +0800

    ng g m charts --routing

---
+++ src/app/charts/charts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ChartsRoutingModule } from './charts-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ChartsRoutingModule
+  ],
+  declarations: []
+})
+export class ChartsModule { }
