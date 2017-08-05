16b3af2e96500051caf36c2238c144cbf3b52aac
Rei Lin <temisrei@gmail.com>
2017-08-05 13:28:14 +0800

    在 AppRouting 設定 charts/flot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,17 @@ import { Page2Component } from './page2/page2.component';
 import { fallbackRoute } from './shared/fallback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards', component: CardsComponent },
+  { path: 'charts',
+    children: [
+      { path: 'flot', component: FlotComponent}
+    ] },
   fallbackRoute
 ];
 
